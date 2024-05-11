fb7a62ed3bf8755d1a97afbf849d4d5aaf403e9c
claes <claes@holmerson.net>
2024-05-11 19:00:55 +0200

    Hard code HDMI port

--- libcec.go
+++ libcec.go
@@ -77,6 +77,7 @@ func cecInit(c *Connection, deviceName string) (C.libcec_connection_t, error) {
 	conf.deviceTypes.types[0] = C.CEC_DEVICE_TYPE_RECORDING_DEVICE
 	conf.callbackParam = unsafe.Pointer(c)
 	conf.bActivateSource = 0
+	conf.iHDMIPort = 4
 	//conf.iButtonReleaseDelayMs = 0
 	//conf.iPhysicalAddress = 0
 	C.setName(conf, C.CString(deviceName))
